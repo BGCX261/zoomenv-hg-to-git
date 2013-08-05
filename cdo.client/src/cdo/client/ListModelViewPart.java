73c8b0896026421fbe0f63827bbc044dc8cc3f18
guevarax <devnull@localhost>
2013-08-05 01:43:27 -0400

    Vista de Arbol, intento de modificacion, pero no persiste

--- cdo.client/src/cdo/client/ListModelViewPart.java
+++ cdo.client/src/cdo/client/ListModelViewPart.java
@@ -1,39 +1,33 @@
 package cdo.client;
 
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
 import org.eclipse.ui.part.ViewPart;
-import org.eclipse.swt.widgets.Table;
-import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.TableColumn;
-import org.eclipse.jface.viewers.TableViewerColumn;
-
 //*******************************************************
-import library.Library;
 import org.eclipse.emf.cdo.net4j.CDONet4jSession;
+import org.eclipse.emf.cdo.transaction.CDOTransaction;
+import org.eclipse.emf.cdo.util.CommitException;
 import org.eclipse.emf.cdo.view.CDOView;
 import org.eclipse.emf.cdo.eresource.CDOResource;
 import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
 
 import java.io.IOException;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Text;
+
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FillLayout;
 //*******************************************************
 
 public class ListModelViewPart extends ViewPart {
-	private Table table;
-
-	//*******************************************************
-    private Library library; //The library instance
-    private CDONet4jSession cdoSession; //The CDO session
-    private Text txtRepositorio;
-    private Text txtModelo;
-    private Text txtModelo_1;
+	private CDONet4jSession cdoSession; //The CDO session
     //*******************************************************
 	
 	public ListModelViewPart() {
@@ -42,94 +36,53 @@ public class ListModelViewPart extends ViewPart {
 
 	@Override
 	public void createPartControl(Composite parent) {
-		String modelo = "";
 		String repositorio ="";
 		//*******************************************************
         //Open CDO session and view
         cdoSession = TestCdoClient.openSession("mem");
         CDOView view = cdoSession.openView();
         CDOResource resource = view.getResource("/myResource");
-        
-        parent.setLayout(new GridLayout(1, false));
-        //*******************************************************
-		/*
-		TableViewer tableViewer = new TableViewer(parent, SWT.BORDER | SWT.FULL_SELECTION);
-		table = tableViewer.getTable();
-		
-		TableViewerColumn tableViewerColumn = new TableViewerColumn(tableViewer, SWT.NONE);
-		TableColumn tblclmnName = tableViewerColumn.getColumn();
-		tblclmnName.setWidth(100);
-		tblclmnName.setText("Name");
-		*/
-		Composite composite = new Composite(parent, SWT.NONE);
-		composite.setLayout(new GridLayout(2, false));
-		composite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-		
-		Label lblRepositorio = new Label(composite, SWT.NONE);
-		lblRepositorio.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
-		lblRepositorio.setText("Repositorio");
-		txtRepositorio = new Text(composite, SWT.BORDER);
-		txtRepositorio.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-        
-        try {
-            //Load resource library
-            resource.load(null);
-            
+        parent.setLayout(new FillLayout());
+		final Tree tree = new Tree(parent, SWT.NONE);
+		TreeItem itemRepositorio = new TreeItem(tree, SWT.NONE);
+            try {
+            	//Load resource repository
+				resource.load(null);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
             
             EList<EObject> modelos = resource.getContents();
             repositorio = resource.getName();
-            txtRepositorio.setText(repositorio);
+            itemRepositorio.setText(repositorio);
             int cont3 = 0;
             for(EObject a: modelos){
             	String o = a.eClass().getName();
-            	
-            	Label lblModelo3 = new Label(composite, SWT.NONE);
-        		lblModelo3.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
-        		
-        		lblModelo3.setText("Modelo "+cont3);
-        		
-        		txtModelo = new Text(composite, SWT.BORDER);
-        		txtModelo.setText(o);
-        		txtModelo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+            	TreeItem itemModelo = new TreeItem(itemRepositorio, SWT.NONE);
+            	itemModelo.setText(o);
         		cont3++;
-            	
-            	//String m = a.eClass().getEStructuralFeatures().get(2).getName();
-            	//String m = a.eClass().getEStructuralFeature(2).getName();
             	EList<EObject> clases = a.eContents();
             	int cont2 = 0;
             	for(EObject b: clases){
-            		EList<EStructuralFeature> cosas = b.eClass().getEAllStructuralFeatures();
             		String n = b.eClass().getName();
-            		Label lblModelo2 = new Label(composite, SWT.NONE);
-            		lblModelo2.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
-            		
-            		lblModelo2.setText("Clase "+cont2);
-            		
-            		txtModelo = new Text(composite, SWT.BORDER);
-            		txtModelo.setText(n);
-            		txtModelo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+            		TreeItem itemClase = new TreeItem(itemModelo, SWT.NONE);
+            		itemClase.setText(n);
             		cont2++;
-            		
-            		//String m = b.eClass().getEStructuralFeature(2).getName();
             		int cont = 0;
-            		for(EStructuralFeature c: cosas){
+            		EList<EAttribute> atributos = b.eClass().getEAllAttributes();
+            		for(EAttribute c: atributos){
             			String m = c.getName();
-            			
-            			Label lblModelo = new Label(composite, SWT.NONE);
-                		lblModelo.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
-                		
-                		lblModelo.setText("Atributo "+cont);
-                		
-                		txtModelo = new Text(composite, SWT.BORDER);
-                		txtModelo.setText(m);
-                		txtModelo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+            			TreeItem itemAtributo = new TreeItem(itemClase, SWT.NONE);
+            			itemAtributo.setText(m);
                 		cont++;
             		}
-            		
-            		
+            		EList<EReference> referencias = b.eClass().getEAllReferences();
+            		for(EStructuralFeature c: referencias){
+            			String m = c.getName();
+            			TreeItem itemAtributo = new TreeItem(itemClase, SWT.NONE);
+            			itemAtributo.setText(m+" (Ref)");
+            		}
             	}
-            	
-            	
             	/*Arbol de contenidos
             	TreeIterator<EObject> clases = a.eAllContents();
             	clases.next(); //autor
@@ -139,31 +92,57 @@ public class ListModelViewPart extends ViewPart {
             	//clases.next(); //error
             	String m = clases.next().eClass().getName();
             	*/
-            	
-            	}
-            
-            
-            
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-		
-		
-		
-		
-		
-		
-		
-		
-	
-
+            }
+            Menu menu = new Menu(tree);
+            MenuItem menuItem = new MenuItem(menu, SWT.NONE);
+            menuItem.setText("Print Element");
+            menuItem.addSelectionListener(new SelectionAdapter() {
+                @Override
+                public void widgetSelected(SelectionEvent event) {
+                	CDOTransaction transaction = cdoSession.openTransaction();
+                    CDOResource resource = transaction.getOrCreateResource("/myResource");
+                	EList<EObject> modelos = resource.getContents();
+                	int cont3 = 0;
+                    for(EObject a: modelos){
+                    	
+                    	EList<EObject> clases = a.eContents();
+                    	int cont2 = 0;
+                    	for(EObject b: clases){
+                    		EList<EAttribute> atributos = b.eClass().getEAllAttributes();
+                    		
+                    		int cont = 0;
+                    		for(EAttribute c: atributos){
+                    			String m = c.getName();
+                    			if(m==tree.getSelection()[0].getText()){
+                    				//resource.getContents().get(cont3).eContents().get(cont2).eClass().getEAllAttributes().get(cont).setName("testOtro");
+                    				EObject nuevo = resource.getContents().get(cont3);
+                    				nuevo.eContents().get(cont2).eClass().getEAllAttributes().get(cont).setName("testOtro");
+                    				resource.getContents().set(cont3, nuevo);
+                    				try {
+										transaction.commit();
+									} catch (CommitException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									}
+                    				System.out.println("Se cambio");
+                    			}
+                    			//cosas.set(cont, c);
+                        		cont++;
+                    		}
+                    		cont2++;
+                    	}
+                    	cont3++;
+                    }
+                    //System.out.println(tree.getSelection()[0].getText());
+                    transaction.close();
+                }
+            });
+            tree.setMenu(menu);
 	}
-
+	
 	@Override
 	public void setFocus() {
 		// TODO Auto-generated method stub
-
 	}
-
 }
+
